983e26d66a7178196ce420f0bf9dc8b9858db398
James Ashley <james@teamdaves.us>
2013-08-24 15:53:19 -0500

    Initial generation: project to experiment with the new version of jogl deployed to clojars by toxi

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject frjogl "0.1.0-SNAPSHOT"
+  :description "Try messing around with JOGL again"
+  :url "http://frereth.com/"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojars.toxi/jogl "2.0.0-rc11"]]
+  :main frjogl.core)
